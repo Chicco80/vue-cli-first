a347072028c81bf61edbb2e5f03265c4c10d84d7
Chicco80 <chicco80lisa@gmail.com>
2022-05-03 17:20:35 +0200

    ok

---
+++ src/components/NewTitle.vue
@@ -0,0 +1,25 @@
+
+<template>
+ <div id="Title">
+   <h1>La mia prima app con Vue CLI!</h1> 
+   <img src="../assets/jumbo.jpg" alt="jumbo">
+  </div>
+    
+</template>
+
+<script>
+
+
+export default {
+  name: 'NewTitle',
+  components: {
+    
+  }
+}
+</script>
+
+<style scoped>
+img{
+    width: 100%;
+}
+</style>
